692b0c5b097d59896ad2b67fc2a3c7d5e9657eca
AaryaDeshpande <52829331+AaryaDeshpande@users.noreply.github.com>
2024-12-09 17:51:29 -0600

    Modified MapFragment such that it immediately (no delay this time) loads and pins all Explore cards' locations upon opening Map fragment

--- app/src/main/java/com/cs407/personitrip/MapFragment.kt
+++ app/src/main/java/com/cs407/personitrip/MapFragment.kt
@@ -24,6 +24,7 @@ class MapFragment : Fragment() {
 
     // Map variables for map values.
     private lateinit var mMap: GoogleMap
+    private var initialLocationSet = false  // Add this flag
     // mDestinationLatLngs is going to be an ArrayList of Loc objects to plot on map.
     private var mDestinationLatLngs: ArrayList<Loc> = ArrayList()
     private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
@@ -58,62 +59,57 @@ class MapFragment : Fragment() {
      */
     private val callback = OnMapReadyCallback { googleMap ->
         Log.d("MapFragment", "onMapReady called")
-        // Map is now available.
         mMap = googleMap
 
-        // code to display markers
-        /* TODO - NOTE for next programmer:
-        I have added placeholders to locations. These are my favorite locations in Madtown.
-        No, I do not plan on "gatekeeping" them - in fact, I would totally share them with others.
-        However, they may not be other's favorite locations
-
-        tl;dr - please remove the placeholders after verifying that other locations are added.
-
-        Thanks.
-        -dhdingwisc
-         */
-        // Picnic Point (43.0898, -89.4151)
-        val locPicnic = Loc(LatLng(43.0898, -89.4151), "Picnic Point")
-        mDestinationLatLngs.add(locPicnic)
-        // Tenney Park (43.0960, -89.3752)
-        mDestinationLatLngs.add(Loc(LatLng(43.0960, -89.3752), "Tenney Park"))
-        // Camp Randall Stadium (43.0679, -89.4178)
-        mDestinationLatLngs.add(Loc(LatLng(43.0679, -89.4178), "Camp Randall Stadium"))
-
-        /* TODO - NOTE for next programmer:
-        Please add necessary programming to add the locations from explore tab (generated from the
-        planned implementation to find locations).
-        While adding necessary programming, please be cautious that locations may need to be added
-        to mDestinationLatLngs outside of the callback, but it has already been initialized as an
-        ArrayList so that it is hopefully easier to go through them.
-
-        Thanks.
-        -dhdingwisc
-         */
-
-        Log.d("MapFragment", "callback - getting shared prefs.")
-        val sharedPrefs = requireContext().getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
-        val cityName = sharedPrefs.getString("selectedCityName", null)
-        val cityLat = sharedPrefs.getString("selectedCityLat", null)?.toDoubleOrNull()
-        val cityLng = sharedPrefs.getString("selectedCityLng", null)?.toDoubleOrNull()
-
-        if (cityName != null && cityLat != null && cityLng != null) {
-            updateLocation(Loc(LatLng(cityLat, cityLng), cityName))
+        // Check if we already have locations
+        val currentLocations = exploreViewModel.locations.value
+        if (currentLocations != null && currentLocations.isNotEmpty()) {
+            // If we have locations, show them immediately
+            updateMapMarkers(currentLocations)
+            initialLocationSet = true
         } else {
-            val defaultLocation = Loc(LatLng(43.0731, -89.4012), "Madison, WI")
-            updateLocation(defaultLocation)
-        }
+            // If no locations yet, show default pin
+            Log.d("MapFragment", "No locations yet, showing default pin")
+            val sharedPrefs = requireContext().getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
+            val cityName = sharedPrefs.getString("selectedCityName", null)
+            val cityLat = sharedPrefs.getString("selectedCityLat", null)?.toDoubleOrNull()
+            val cityLng = sharedPrefs.getString("selectedCityLng", null)?.toDoubleOrNull()
 
-        // Loop through all locations in AL to add to the map.
-        for (i in mDestinationLatLngs.indices) {
-            mMap.addMarker(
-                MarkerOptions()
-                    .position(mDestinationLatLngs[i].coordinates)
-                    .title(mDestinationLatLngs[i].name)
-            )
+            if (cityName != null && cityLat != null && cityLng != null) {
+                updateLocation(Loc(LatLng(cityLat, cityLng), cityName))
+            } else {
+                val defaultLocation = Loc(LatLng(43.0731, -89.4012), "Madison, WI")
+                updateLocation(defaultLocation)
+            }
         }
-        // Zoom to last location.
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDestinationLatLngs[mDestinationLatLngs.size - 1].coordinates, 10f))
+    }
+
+    private fun updateMapMarkers(locations: List<LatLng>) {
+        if (::mMap.isInitialized) {
+            mMap.clear()  // Clear existing markers
+
+            locations.forEach { latLng ->
+                mMap.addMarker(
+                    MarkerOptions()
+                        .position(latLng)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+                )
+            }
+
+            // Zoom to show all markers
+            if (locations.isNotEmpty()) {
+                val builder = LatLngBounds.Builder()
+                locations.forEach { builder.include(it) }
+                val bounds = builder.build()
+                try {
+                    val padding = 100
+                    val cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, padding)
+                    mMap.animateCamera(cameraUpdate)
+                } catch (e: Exception) {
+                    Log.e("MapFragment", "Error animating camera: ${e.message}")
+                }
+            }
+        }
     }
 
     override fun onCreateView(
@@ -137,39 +133,46 @@ class MapFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         exploreViewModel.locations.observe(viewLifecycleOwner) { locations ->
             Log.d("MapFragment", "Received locations: $locations")
-            if (::mMap.isInitialized) {
-                // Clear existing markers
-                mMap.clear()
-
-                // Add new markers
-                locations.forEach { latLng ->
-                    mMap.addMarker(
-                        MarkerOptions()
-                            .position(latLng)
-                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
-                    )
-                }
-
-                // Zoom to fit all markers if there are any
-                if (locations.isNotEmpty()) {
-                    val builder = LatLngBounds.Builder()
-                    locations.forEach { builder.include(it) }
-                    val bounds = builder.build()
-
-                    try {
-                        val padding = 100 // pixels
-                        val cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, padding)
-                        mMap.animateCamera(cameraUpdate)
-                    } catch (e: Exception) {
-                        Log.e("MapFragment", "Error animating camera: ${e.message}")
-                    }
-                } else {
-                    Log.e("MapFragment", "Map not initialized yet")
-                }
-            }
+            updateMapMarkers(locations)
+            initialLocationSet = true
         }
+
+//        exploreViewModel.locations.observe(viewLifecycleOwner) { locations ->
+//            Log.d("MapFragment", "Received locations: $locations")
+//            if (::mMap.isInitialized) {
+//                // Clear existing markers
+//                mMap.clear()
+//
+//                // Add new markers
+//                locations.forEach { latLng ->
+//                    mMap.addMarker(
+//                        MarkerOptions()
+//                            .position(latLng)
+//                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+//                    )
+//                }
+//
+//                // Zoom to fit all markers if there are any
+//                if (locations.isNotEmpty()) {
+//                    val builder = LatLngBounds.Builder()
+//                    locations.forEach { builder.include(it) }
+//                    val bounds = builder.build()
+//
+//                    try {
+//                        val padding = 100 // pixels
+//                        val cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, padding)
+//                        mMap.animateCamera(cameraUpdate)
+//                    } catch (e: Exception) {
+//                        Log.e("MapFragment", "Error animating camera: ${e.message}")
+//                    }
+//                } else {
+//                    Log.e("MapFragment", "Map not initialized yet")
+//                }
+//            }
+//        }
 //        val mapFragment = childFragmentManager.findFragmentById(R.id.map_frag_display) as SupportMapFragment?
 //        mapFragment?.getMapAsync(callback)
     }
